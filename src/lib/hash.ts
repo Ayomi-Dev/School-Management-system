b13d1db80e06f7f5a014795e81a46a13b95dbbfe
Ayomide <ayomideolakunlea1@gmail.com>
2026-03-27 17:22:44 +0100

    Added comments to code blocks in the hash.ts file for code readability

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -2,22 +2,29 @@ import bcrypt from "bcrypt";
 import crypto from 'crypto';
 
 
-const SALT_ROUNDS = 12;
+const SALT_ROUNDS = 12; //controls how many times bcrypt processes user's password
+
+
 
 export const hashPassword = async(password: string): Promise<string> => {
-    return bcrypt.hash(password, SALT_ROUNDS)
+    //Takes the plain-text password and runs it through bcrypt's hashing algorithm 2¹² (4,096) times
+    //adding the salt) automatically to make it unique and resistant to rainbow table attacks.
+    //  The resulting hash is what gets stored in the database, not the original password.
+    return bcrypt.hash(password, SALT_ROUNDS) 
 }
 
 export async function verifyPassword( 
     password: string, hashedPassword: string
 ): Promise<boolean> {
-    return bcrypt.compare(password, hashedPassword)
+    // Takes a plain-text password and a stored hash, re-hashes the plain password using the same salt embedded inside the hash, 
+    // then compares the two. Returns true if they match, false if not.
+    return bcrypt.compare(password, hashedPassword) 
 }
 
 export const hashToken = async(token: string): Promise<string> => {
-    return crypto.createHash("sha256")
-    .update(token)
-    .digest("hex")
+    return crypto.createHash("sha256") //Creates a new hash object using the SHA-256 algorithm, which is a cryptographic hash function that produces a fixed-size 256-bit (32-byte) hash value.
+    .update(token)  //Feeds the token string into the hasher.
+    .digest("hex")  //Performs the hashing operation and outputs the result as a hexadecimal string, which is what gets stored in the database.
 }
 
 export const generateOTP = (): string => {
@@ -26,7 +33,9 @@ export const generateOTP = (): string => {
 
 export const generateReceiptNumber = (): string => {
   const date = new Date();
-  const prefix = `RCP${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, "0")}`;
-  const suffix = crypto.randomBytes(3).toString("hex").toUpperCase();
-  return `${prefix}-${suffix}`;
+
+//   Extracts the 4-digit year,  gets the month,  ensures the month is always 2 digits (e.g., "01" for January), and constructs a prefix in the format "RCPYYYYMM". Then, 
+  const prefix = `RCP${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, "0")}`; 
+  const suffix = crypto.randomBytes(3).toString("hex").toUpperCase(); //generates a random 6-character hexadecimal string as a suffix and converts it to uppercase
+  return `${prefix}-${suffix}`; //combines both parts with a hyphen to create the final receipt number.
 }
